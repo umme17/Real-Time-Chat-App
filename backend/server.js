0a489524d2d32ba84c18115748ff2e1e05131de8
umme17 <ummehabiba17805hk@gmail.com>
2025-05-10 17:04:04 +0600

    Modification of  server.js  for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
@@ -11,6 +12,8 @@ import {app, server} from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
@@ -23,6 +26,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,() => {
     console.log(`server Running on port  ${PORT}`);
